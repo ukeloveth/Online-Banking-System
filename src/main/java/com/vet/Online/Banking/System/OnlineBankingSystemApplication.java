0750a71e2abae9cf69fa93d6820251820155ff83
ukeloveth <ukeloveth247@gmail.com>
2023-11-01 08:56:10 +0100

    Online Banking System

---
+++ src/main/java/com/vet/Online/Banking/System/OnlineBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vet.Online.Banking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingSystemApplication.class, args);
+	}
+
+}
